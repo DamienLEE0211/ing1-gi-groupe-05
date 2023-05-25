1b40d1163c7ff5c8994f959b3f27413f3b7f7ad7
Clara Layus <layusclara@cy-tech.fr>
2023-05-25 16:43:31 +0200

    refactor project

--- front/src/main.js
+++ front/src/main.js
@@ -1,8 +1,5 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
 
-new Vue({
-    router,
-    render: (h) => h(App),
-}).$mount('#app');
+createApp(App).use(router).mount('#app')
--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,35 +1,25 @@
-// Import Vue Router and the components
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import FooterUser from '../components/FooterUser.vue';
-import HeaderUser from '../components/HeaderUser.vue';
-import MidUser from '../components/MidUser.vue';
-import P1Capitaine from '../components/P1Capitaine.vue';
-import P1Participant from '../components/P1Participant.vue';
-import Q1mid from '../components/Q1mid.vue';
-import U1MidBot from '../components/U1MidBot.vue';
-import U1MidTop from '../components/U1MidTop.vue';
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
-// Use Vue Router plugin
-Vue.use(VueRouter);
-
-// Create routes
 const routes = [
-    { path: '/', component: MidUser },
-    { path: '/header', component: HeaderUser },
-    { path: '/footer', component: FooterUser },
-    { path: '/p1capitaine', component: P1Capitaine },
-    { path: '/p1participant', component: P1Participant },
-    { path: '/q1mid', component: Q1mid },
-    { path: '/u1midbot', component: U1MidBot },
-    { path: '/u1midtop', component: U1MidTop },
-];
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
 
-// Create the router instance
-const router = new VueRouter({
-    mode: 'history',
-    routes,
-});
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes
+})
 
-// Export the router instance
-export default router;
+export default router
