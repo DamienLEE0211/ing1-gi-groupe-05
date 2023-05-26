b8b2fd85b9ed7a1be6ad0fb8397985e4f61e7352
Dongwon <ehddoswn@gmail.com>
2023-05-26 14:14:16 +0200

    new code for U1 page

---
+++ front/src/components/U1MidBot.vue
@@ -0,0 +1,85 @@
+<script>
+export default {
+  data() {
+    return {
+
+          project1: {
+            image: "", // Provide the image source
+            name: "Nom", // Replace with the actual name
+            dates: "Dates" // Replace with the actual dates
+          },
+          project2: {
+            image: "", // Provide the image source
+            name: "Nom", // Replace with the actual name
+            dates: "Dates" // Replace with the actual dates
+          },
+          challengeText: "S'inscrire à nouvelle challenge",
+        }
+      }
+
+};
+
+</script>
+
+<template>
+  <div id="second-mid">
+    <div id="div-project">
+      <p id="txt-mes-projets">Mes projets</p>
+      <div id="Three-box-contains">
+        <div class="each-project">
+          <img :src="project1.image" alt="Image projet">
+          <p>{{ project1.name }}</p>
+          <p>{{ project1.dates }}</p>
+        </div>
+        <div class="each-project">
+          <img :src="project2.image" alt="Image projet">
+          <p>{{ project2.name }}</p>
+          <p>{{ project2.dates }}</p>
+        </div>
+        <div class="each-project">
+          <p>{{ challengeText }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+#second-mid{
+  background-color: #B2D3C2;
+  height: 50%;
+  width: 80%;
+}
+
+#div-project{
+  text-align: center;
+  margin-top: -1%;
+  background-color: white;
+  height: 100%;
+  width: 100%;
+
+  margin-left: 2%;
+}
+
+#txt-mes-projets{
+  font-size: 30px;
+  font-weight: bold;
+}
+
+#Three-box-contains{
+  padding-left: 5em;
+  padding-right: 5em;
+  margin-top: 3em;
+  display: flex;
+  justify-content: space-between;
+  justify-items: center;
+}
+.each-project{
+  padding-top: 1em;
+  justify-items: center;
+  width: 10em;
+  height: 15em;
+  background-color: #B2D3C2;
+  display: block;
+}
+</style>
---
+++ front/src/components/U1MidTop.vue
@@ -0,0 +1,84 @@
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {text: 'Accueil', url: 'acceuil.html'},
+        {text: 'QCM', url: 'qcm.html'},
+        {text: 'Data Challenge', url: 'datachallenge.html'}
+      ],
+      firstName: 'Prénom',
+      lastName: 'Nom',
+      username: 'Utilisateur',
+      school: 'Ecole / Entreprise',
+    }
+  },
+  methods: {
+    buttonClicked() {
+      alert('Button clicked!');
+    }
+  },
+
+};
+
+
+</script>
+
+<template>
+  <span id="containers">
+        <h1 id="info-perso">Informations personnelles</h1>
+        <div id="info-user">
+          <p>{{ firstName }}</p>
+          <p>{{ lastName }}</p>
+          <p>{{ username }}</p>
+        </div>
+
+        <div id="info-ecole">
+          <p>{{ school }}</p>
+        </div>
+
+        <div id="div-btn">
+          <button id="BTN-modif-info" @click="buttonClicked">Modifier les informations</button>
+          <button id="BTN-cherche-chall" @click="buttonClicked">Chercher un challenge</button>
+        </div>
+  </span>
+</template>
+
+<style>
+  #containers{
+    width: 100%;
+    margin-left: 4em;
+  }
+  #info-perso{
+    color: white;
+  }
+  #info-user{
+    display: flex;
+    color: white;
+    font-size: 18px;
+  }
+  #info-user p{
+    margin-right: 1em;
+  }
+
+  #info-ecole{
+    color: white;
+    font-size: 18px;
+  }
+  button{
+    margin-right: 2em;
+  }
+  #BTN-modif-info,#BTN-cherche-chall{
+
+    background-color: #B2D3C2;
+    color: white;
+    font-size: 18px;
+    padding: 10px 20px ;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+    width: 250px;
+    margin-top: 4em;
+    transform: translate(0, -80%);
+  }
+</style>
