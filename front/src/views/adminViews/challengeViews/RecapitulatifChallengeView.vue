fc791d6c7f205eab5a296dfea29aad9e6918f570
Clara Layus <layusclara@cy-tech.fr>
2023-05-28 23:05:34 +0200

    front: part accueil complete

--- front/src/views/adminViews/challengeViews/RecapitulatifChallengeView.vue
+++ front/src/views/adminViews/challengeViews/RecapitulatifChallengeView.vue
@@ -1,2 +1,17 @@
 <template>
+  <div class="about">
+    <h1>Page du challenge [Ad3]</h1>
+  </div>
 </template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
+<script setup>
+</script>
