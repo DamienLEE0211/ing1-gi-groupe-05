36179c6d5b4897863ae8f7ba329e52433c40235a
Dongwon <ehddoswn@gmail.com>
2023-05-31 11:55:11 +0200

    participantViews is coded for accueil,classement,AccueilParticipant

--- front/src/views/participantViews/AccueilParticipant.vue
+++ front/src/views/participantViews/AccueilParticipant.vue
@@ -1,17 +1,158 @@
 <template>
-  <div class="about">
-    <h1>Page d'accueil du participant [U1]</h1>
+  <div id="mid">
+    <span id="containers">
+      <h1 id="info-perso">Informations personnelles</h1>
+      <div id="info-user">
+        <p>{{ firstName }}</p>
+        <p>{{ lastName }}</p>
+        <p>{{ username }}</p>
+      </div>
+
+      <div id="info-ecole">
+        <p>{{ school }}</p>
+      </div>
+
+      <div id="div-btn">
+        <button id="BTN-modif-info" @click="buttonClicked">Modifier les informations</button>
+        <button id="BTN-cherche-chall" @click="buttonClicked">Chercher un challenge</button>
+      </div>
+    </span>
+
+    <div id="second-mid">
+      <div id="div-project">
+        <p id="txt-mes-projets">Mes projets</p>
+        <div id="Three-box-contains">
+          <div class="each-project">
+            <img :src="project1.image" alt="Image projet">
+            <p>{{ project1.name }}</p>
+            <p>{{ project1.dates }}</p>
+          </div>
+          <div class="each-project">
+            <img :src="project2.image" alt="Image projet">
+            <p>{{ project2.name }}</p>
+            <p>{{ project2.dates }}</p>
+          </div>
+          <div class="each-project">
+            <p>{{ challengeText }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+#containers{
+  width: 100%;
+  margin-left: 4em;
+}
+#info-perso{
+  color: white;
+}
+#info-user{
+  display: flex;
+  color: white;
+  font-size: 18px;
+}
+#info-user p{
+  margin-right: 1em;
+}
+
+
+#info-ecole{
+  color: white;
+  font-size: 18px;
+}
+
+
+button{
+  margin-right: 2em;
+}
+
+#BTN-modif-info,#BTN-cherche-chall{
+
+  background-color: #B2D3C2;
+  color: white;
+  font-size: 18px;
+  padding: 10px 20px ;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  width: 250px;
+  margin-top: 4em;
+  transform: translate(0, -80%);
+}
+
+#second-mid{
+  background-color: #B2D3C2;
+  height: 50%;
+  width: 80%;
+}
+#txt-mes-projets{
+  margin: 0;
+  padding-top: 1em;
+}
+
+#div-project{
+  text-align: center;
+  background-color: white;
+  height: 100%;
+  width: 100%;
+  clip-path: polygon(0 3%, 100% 0%, 100% 97%, 0% 100%);
+}
+
+#txt-mes-projets{
+  font-size: 30px;
+  font-weight: bold;
+}
+
+#Three-box-contains{
+  padding-left: 5em;
+  padding-right: 5em;
+  margin-top: 3em;
+  display: flex;
+  justify-content: space-between;
+  justify-items: center;
+}
+.each-project{
+  padding-top: 1em;
+  justify-items: center;
+  width: 10em;
+  height: 15em;
+  background-color: #B2D3C2;
+  display: block;
 }
 </style>
-<script setup>
+<script>
+Vue.createApp({
+  data() {
+    return {
+
+      firstName: 'Prénom',
+      lastName: 'Nom',
+      username: 'Utilisateur',
+      school: 'Ecole / Entreprise',
+
+      project1: {
+        image: "", // Provide the image source
+        name: "Nom", // Replace with the actual name
+        dates: "Dates" // Replace with the actual dates
+      },
+      project2: {
+        image: "", // Provide the image source
+        name: "Nom", // Replace with the actual name
+        dates: "Dates" // Replace with the actual dates
+      },
+      challengeText: "S'inscrire à nouvelle challenge",
+
+
+    };
+  },
+  methods: {
+    buttonClicked() {
+      alert('Button clicked!');
+    }
+  },
+}).mount('body');
 </script>
--- front/src/views/participantViews/projetViews/AccueilProjet.vue
+++ front/src/views/participantViews/projetViews/AccueilProjet.vue
@@ -1,17 +1,220 @@
 <template>
-  <div class="about">
-    <h1>Page d'accueil du projet du participant [P1]</h1>
+  <div class="P1-mid">
+    <div id="P1-midtop">
+      <div id="first-block">
+        <div id="img-block">
+          <img :src="project1.image" alt="Image projet">
+        </div>
+
+        <div id="ressources">
+          <h2>Ressources:</h2>
+          <a v-for="(lien, index) in liens" :key="index" :href="lien.url">{{ lien.label }}</a>
+        </div>
+      </div>
+
+      <div id="second-block">
+        <h1 id="text-projet">Nom projet</h1>
+        <p id="description">Description du projet</p>
+        <button id="BTN-question" @click="buttonClicked('Questionnaires')">Questionnaires</button>
+      </div>
+      <div id="third-block">
+        <div id="inside-third-block">
+          <h1 id="contact">Contact</h1>
+          <button id="BTN-message" @click="buttonClicked('Message')">Message</button>
+          <h1>Equipe</h1>
+          <p v-if="userRole === 1">nom capitaine</p>
+          <ul id="coequipe-list">
+            <li v-for="(coequipe, index) in coequipes" :key="index">
+              {{ coequipe }}
+            </li>
+          </ul>
+          <button v-if="userRole === 1" id="BTN-invitation" @click="buttonClicked('Inviter')">Inviter</button>
+          <a v-if="userRole === 1" id="supp-team" href="">supprimer l'équipe</a>
+        </div>
+      </div>
+    </div>
+    <div id="P1-midBot">
+      <div id="lien-code">
+        <p>Lien hébergement du code (gitlab):</p>
+        <div id="lien-git">
+          <a href="http://www.github.com">git</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      userRole :0,
+
+      project1: {
+        image: "path/to/image.jpg",
+        name: "Nom",
+        dates: "Dates"
+      },
+
+      coequipes: [
+        "coéquipé 2",
+        "coéquipé 3"
+      ],
+      liens: [
+        { label: "lien 1", url: "" },
+        { label: "lien 2", url: "" },
+        { label: "lien 3", url: "" },
+        { label: "lien 4", url: "" },
+      ],
+    };
+  },
+  methods: {
+    buttonClicked(action) {
+      console.log(action + " button clicked!");
+    },
+  },
+};
+</script>
+
 <style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
+.P1-mid{
+  gap: 0;
+  background-color: #0E8769;
+  height: 100vh;
+
+}
+#P1-midtop{
+  height: 80%;
+  justify-content: space-between;
+  display: flex;
+}
+#P1-midBot{
+  height: 20%;
+}
+#first-block{
+  background-color: #0E8769;
+  display: block;
+  width: 15%;
+  height: 70%;
+  margin-left: 3%;
+}
+#second-block {
+  margin-top: 2em;
+  background-color: #0E8769;
+  display: block;
+  width: 15%;
+  height: 70%;
+  justify-content: center;
+  justify-items: center;
+  position: relative;
+}
+#BTN-question{
+  width: 80%;
+  margin-right: 0;
+  border-radius: 10px;
+  height: 3em;
+  border-color: inherit;
+  position: absolute;
+  bottom: 0;
+}
+
+#third-block{
+  margin-top: 2em;
+  background-color: #B2D3C2;
+  width: 15%;
+  margin-right: 3%;
+  height: 70%;
+  justify-content: center;
+  align-items: center;
+  display: flex;
+}
+
+
+#img-block{
+  background-color: #D3D4D9;
+  margin-top: 2em;
+  height: 20%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#ressources{
+  border-radius: 30px;
+  margin-top: 4em;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #B2D3C2;
+  height: 60%;
+}
+
+#ressources a {
+  display: block;
+  margin-bottom: 10px;
+  color: inherit;
+  text-decoration: none;
+  margin-right: 0;
+}
+#ressources a:hover {
+  color: #B9B6B7;
+}
+
+#text-projet, #description{
+  color: white;
+}
+
+#inside-third-block{
+  background-color: white;
+  height: 98%;
+  width: 100%;
+  padding-top: 5%;
+  clip-path: polygon(0 2%, 100% 0%, 100% 98%, 0% 100%);
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+#contact{
+  margin-top: 0;
+}
+
+#BTN-message,#BTN-invitation{
+  margin-right: 0;
+  background-color: #0E8769;
+  width: 70%;
+  height: 3em;
+  border-radius: 10px;
+  color: white;
+  border-color: inherit;
+}
+
+#supp-team{
+  margin-top: 1em;
+  margin-right: 0;
+}
+
+#lien-code{
+  width: 60%;
+  height: 80%;
+  background-color: white;
+  border-radius: 10px;
+  margin-left: 3%;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+  justify-items: center;
+}
+#lien-code p{
+  padding-top: 1em;
+  font-size: 20px;
+}
+#lien-git{
+  height: 30%;
+  width: 100%;
+  background-color: #B2D3C2;
+  margin-right: 0;
+  align-items: center;
 }
 </style>
-<script setup>
-</script>
